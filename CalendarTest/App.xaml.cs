45425040344096f42dd0216c2dbd33426790eee5
Toni Vucic <tonivuc@gmail.com>
2020-06-02 15:18:39 +0200

    Added project files

---
+++ CalendarTest/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CalendarTest.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalendarTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjY2MjM5QDMxMzgyZTMxMmUzMFgrS1JiNHBqd2U0cUhUdDR3dVlEUEdrMUlSeU9qZ0xqR2E5bDduL0tWS0k9");
+            InitializeComponent();
+            MainPage = new SchedulePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CalendarTest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Syncfusion.SfSchedule.XForms;
+
+namespace CalendarTest.ViewModels
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ScheduleAppointmentCollection AppointmentCollection { get; set; }
+        public DayViewSettings DayViewSettings { get; set; }
+
+        public MainPageViewModel()
+        {
+            AppointmentCollection = new ScheduleAppointmentCollection();
+            SetDayLabelFormatToMinutes();
+            doStuff();
+        }
+
+        private void SetDayLabelFormatToMinutes()
+        {
+            DayViewSettings = new DayViewSettings();
+            DayLabelSettings dayLabelSettings = new DayLabelSettings();
+            dayLabelSettings.TimeFormat = "hh mm";
+            DayViewSettings.DayLabelSettings = dayLabelSettings;
+        }
+        private void doStuff()
+        {
+            //Creating new event   
+            ScheduleAppointment clientMeeting = new ScheduleAppointment();   
+            DateTime currentDate = DateTime.Now;   
+            DateTime startTime = new DateTime (currentDate.Year,currentDate.Month,currentDate.Day, 10, 0, 0);    
+            DateTime endTime = new DateTime (currentDate.Year, currentDate.Month,currentDate.Day, 12, 0, 0);   
+            clientMeeting.StartTime = startTime;   
+            clientMeeting.EndTime = endTime;   
+            clientMeeting.Color = Color.Blue;   
+            clientMeeting.Subject = "ClientMeeting";
+            clientMeeting.Notes = "We will discuss eating porridge or pancakes for lunch";
+            clientMeeting.Location = "Room 402";
+
+            AppointmentCollection.Add(clientMeeting);
+
+            ScheduleAppointment clientMeeting2 = new ScheduleAppointment();   
+            currentDate = DateTime.Now;   
+            startTime = new DateTime (currentDate.Year,currentDate.Month,currentDate.Day, 10, 0, 0);    
+            endTime = new DateTime (currentDate.Year, currentDate.Month,currentDate.Day, 12, 0, 0);   
+            clientMeeting2.StartTime = startTime;   
+            clientMeeting2.EndTime = endTime;   
+            clientMeeting2.Color = Color.Red;   
+            clientMeeting2.Subject = "Othermeeting";
+            clientMeeting2.Notes = "We will discuss eating porridge or pancakes for lunch";
+            clientMeeting2.Location = "Room 402";
+
+            AppointmentCollection.Add(clientMeeting2);
+        }
+    }
+}
