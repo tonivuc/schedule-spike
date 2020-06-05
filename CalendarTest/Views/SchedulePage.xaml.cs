a2580ffb8b5d6a917b13e98d78baa680c59d699f
Toni Vucic <tonivuc@gmail.com>
2020-06-05 12:28:37 +0200

    Added tap event recognizers to move the calendars

--- CalendarTest/Views/SchedulePage.xaml.cs
+++ CalendarTest/Views/SchedulePage.xaml.cs
@@ -22,6 +22,18 @@ namespace CalendarTest.Views
         private void Button_OnClicked(object sender, EventArgs e)
         {
             App.Current.MainPage.DisplayAlert("Alert", "Alert alert code red!","No worries!");
+            DateTime currentDate = DateTime.Now;   
+            DateTime startTime = new DateTime (currentDate.Year,currentDate.Month,currentDate.Day, 14, 0, 0);
+            SfSchedule2.MoveToDate = startTime;
+        }
+
+        private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+        {
+            DateTime currentDate = DateTime.Now;   
+            DateTime startTime = new DateTime (currentDate.Year,currentDate.Month,currentDate.Day, 14, 0, 0);
+            var date = SfSchedule.SelectedDate;
+            SfSchedule.MoveToDate = date.Value;
+
         }
     }
 }
